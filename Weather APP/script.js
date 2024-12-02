892407d239ff9bcdfc9e8b3babd265f9abe3da63
Muhammad Aqib Aziz <m.aqib@fitwellhub.com>
2024-12-02 13:36:51 +0500

    Add files via upload

---
+++ Weather APP/script.js
@@ -0,0 +1,9 @@
+// const apikey="e4d7b519651e20d966d0ba4a790c37f0";
+// const apiurl="https://api.openweathermap.org/data/2.5/weather?units=metric";
+
+// async function checkweather() {
+//      const  response=await fetch(apiurl +`&appid=${apikey}`);
+//      var  data=await response.json();
+//      console.log(data);
+//     }
+//     checkweather();
